3c65dce7e2bb6357316da90dff5d250b6df4246d
Fabiola Espinoza <fabi9414@hotmail.com>
2016-03-23 15:09:03 -0600

    Creación del controller

---
+++ SACD_Controlador/ProfesManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SACD_Modelo;
+
+namespace SACD_Controlador
+{
+    public class ProfesManager
+    {
+        //crear profesor
+        public void crear()
+        {
+
+        }
+
+        //eliminar profesor
+        public void eliminar()
+        {
+
+        }
+
+        //editar profesor
+        public void editar()
+        {
+
+        }
+
+        //buscar profesor
+        public Profesor buscar(int id)
+        {
+            return null;
+        }
+
+        //obtener lista de profesores registrados
+        public List<Profesor> listar()
+        {
+            return null;
+        }
+
+        //calcular horas mín. laborales
+        public int calcHorasMin(Profesor pProfesor)
+        {
+            return 0;
+        }
+        
+    }
+}
