edf178e0ca063703d44d1d5fe3820bdde1bb0b99
Jhoel Marenco Godoy <jhoelmg@hotmail.com>
2016-04-29 17:51:03 -0600

    Ventanas de Plazas Listas

---
+++ SACD/Páginas/EditarPlaza.xaml.cs
@@ -0,0 +1,42 @@
+﻿using SACD_Controlador;
+using SACD_Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SACD.Páginas
+{
+    /// <summary>
+    /// Lógica de interacción para Reportes.xaml
+    /// </summary>
+    public partial class EditarPlaza : Page
+    {
+        public EditarPlaza()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Aceptar_Click(object sender, RoutedEventArgs e)
+        {
+            if(tbxNumero.Text != "" && tbxPorcentaje.Text != "")
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("No puede dejar ningún espacio en blanco.");
+            }
+        }
+    }
+}
---
+++ SACD/Páginas/EliminarPlaza.xaml.cs
@@ -0,0 +1,35 @@
+﻿using SACD_Controlador;
+using SACD_Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SACD.Páginas
+{
+    /// <summary>
+    /// Lógica de interacción para Reportes.xaml
+    /// </summary>
+    public partial class EliminarPlaza : Page
+    {
+        public EliminarPlaza()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Aceptar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
